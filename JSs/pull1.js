b1d9f23a7b637351ed30c4058d3a9ee935a5d0fb
TheSebitzu <sebibonea@gmail.com>
2024-09-16 06:42:32 +0300

    change hrefs and finish all workout files
    need to style the site and make history an actual table. also add ex names instead of ex1, ex2 ...

---
+++ JSs/pull1.js
@@ -0,0 +1,37 @@
+let pull1day = {
+    "ex1": {
+        "kg": 0,
+        "reps": 0
+    },
+    "ex2": {
+        "kg": 0,
+        "reps": 0
+    },
+    "ex3": {
+        "kg": 0,
+        "reps": 0
+    },
+    "ex4": {
+        "kg": 0,
+        "reps": 0
+    },
+    "ex5": {
+        "kg": 0,
+        "reps": 0
+    },
+    "ex6": {
+        "kg": 0,
+        "reps": 0
+    },
+    "ex7": {
+        "kg": 0,
+        "reps": 0
+    }
+
+};
+
+let workouts = JSON.parse(localStorage.workouts);
+workouts.pull1.push(pull1day);
+localStorage.workouts = JSON.stringify(workouts);
+
+console.log(localStorage.workouts);
---
+++ JSs/push2.js
@@ -0,0 +1,37 @@
+let push2day = {
+    "ex1": {
+        "kg": 0,
+        "reps": 0
+    },
+    "ex2": {
+        "kg": 0,
+        "reps": 0
+    },
+    "ex3": {
+        "kg": 0,
+        "reps": 0
+    },
+    "ex4": {
+        "kg": 0,
+        "reps": 0
+    },
+    "ex5": {
+        "kg": 0,
+        "reps": 0
+    },
+    "ex6": {
+        "kg": 0,
+        "reps": 0
+    },
+    "ex7": {
+        "kg": 0,
+        "reps": 0
+    }
+
+};
+
+let workouts = JSON.parse(localStorage.workouts);
+workouts.push2.push(push2day);
+localStorage.workouts = JSON.stringify(workouts);
+
+console.log(localStorage.workouts);
