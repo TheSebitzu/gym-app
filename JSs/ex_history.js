73b198f2e2d5af836a2981f67e79b8c5360e1e48
TheSebitzu <sebibonea@gmail.com>
2024-09-16 07:15:45 +0300

    added better history (only shows first workout)
    need to modify in ex_history.js (line 6: that 0 needs to be a dinamic variable)

--- JSs/ex_history.js
+++ JSs/ex_history.js
@@ -1,3 +1,27 @@
 let pushed = localStorage.pushed;
 let workouts = JSON.parse(localStorage.workouts);
-document.getElementById("history").innerHTML = JSON.stringify(workouts[pushed]);
+
+// document.getElementById("history").innerHTML = JSON.stringify(workouts[pushed]);
+
+const exercises = workouts[pushed][0]; //trb inlocuit acel 0
+console.log(exercises);
+
+const tableBody = document.getElementById("history");
+
+for (const [exercise, details] of Object.entries(exercises)) {
+    const row = document.createElement("tr");
+
+    const exerciseCell = document.createElement("td");
+    exerciseCell.textContent = exercise;
+    row.appendChild(exerciseCell);
+
+    const weightCell = document.createElement("td");
+    weightCell.textContent = details.kg;
+    row.appendChild(weightCell);
+
+    const repsCell = document.createElement("td");
+    repsCell.textContent = details.reps;
+    row.appendChild(repsCell);
+
+    tableBody.appendChild(row);
+} 
